0ba082e82b963a3fb11f768f9faa996930fd56fe
Berkay Atakan İhtiyar <26187453+atakanihtiyar@users.noreply.github.com>
2021-08-16 00:15:02 +0300

    Convert eventScreener to passageScreener

---
+++ Assets/Scripts/Event/PassageScreener.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PassageScreener : MonoBehaviour
+{
+    public TMP_Text narrator;
+    private ChoiceButton[] choiceButtons;
+
+    private void Awake()
+    {
+        choiceButtons = GetComponentsInChildren<ChoiceButton>();
+    }
+
+    public void UpdateUI(Passage passage)
+    {
+        narrator.text = passage.text;
+
+        choiceButtons[0].UpdateText(passage.answer1);
+
+        if (passage.answer2 == "")
+        {
+            choiceButtons[1].SetActivate(false);
+        }
+        else
+        {
+            choiceButtons[1].SetActivate(true);
+            choiceButtons[1].UpdateText(passage.answer2);
+        }
+    }
+}

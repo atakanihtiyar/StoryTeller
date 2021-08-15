c9c57b3b27567ac2d0e0985c4bb52ec0ec77eee2
Berkay Atakan İhtiyar <26187453+atakanihtiyar@users.noreply.github.com>
2021-08-16 00:38:55 +0300

    Turn EventNavigator to PassageNavigator except rewind and update PassageNavigator script on object

---
+++ Assets/Scripts/Event/PassageNavigator.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PassageNavigator : MonoBehaviour
+{
+    public TextAsset jsonPath;
+
+    public Passages rootOfPassages;
+    public int currentIndex;
+
+    public PassageScreener passageScreener;
+
+    private void Start()
+    {
+        rootOfPassages = JsonUtility.FromJson<Passages>(jsonPath.text);
+
+        currentIndex = rootOfPassages.passages.FindIndex(x => x.tags == "START");
+        passageScreener.UpdateUI(rootOfPassages.passages[currentIndex]);
+    }
+
+    public void GoNextEvent(int selectedChoiceIndex)
+    {
+        //EventLogger.Instance.PushEvent(currentEvent);
+        if (selectedChoiceIndex == 0)
+        {
+            int newIndex = rootOfPassages.passages.FindIndex(x => x.title == rootOfPassages.passages[currentIndex].to_name1);
+            passageScreener.UpdateUI(rootOfPassages.passages[newIndex]);
+            currentIndex = newIndex;
+        }
+        else if (selectedChoiceIndex == 1)
+        {
+            int newIndex = rootOfPassages.passages.FindIndex(x => x.title == rootOfPassages.passages[currentIndex].to_name2);
+            passageScreener.UpdateUI(rootOfPassages.passages[newIndex]);
+            currentIndex = newIndex;
+        }
+
+    }
+
+    public void RewindButton()
+    {
+        //if (EventLogger.Instance.Logs.Count == 0) return;
+
+        //Event oldEvent = EventLogger.Instance.PopEvent();
+        //currentEvent = oldEvent;
+        //eventScreener.UpdateUI(currentEvent);
+    }
+}

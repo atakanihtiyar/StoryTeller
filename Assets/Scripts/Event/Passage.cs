3979c15da45eb0beaf1cf9b9f0d1e99b7434730c
Berkay Atakan İhtiyar <26187453+atakanihtiyar@users.noreply.github.com>
2021-08-15 23:16:28 +0300

    Change name event to passage and add passage array to seperate passages script

---
+++ Assets/Scripts/Event/Passage.cs
@@ -0,0 +1,11 @@
+﻿[System.Serializable]
+public class Passage
+{
+    public string title;
+    public string tags;
+    public string text;
+    public string answer1;
+    public string to_name1;
+    public string answer2;
+    public string to_name2;
+}
